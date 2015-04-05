157484e58826518b0fe5c08e9d1dacd245d1ecbd
Kevin Chen <kvchen@berkeley.edu>
2015-04-05 11:46:15 -0700

    Readd plugins for demo

--- plugins/gbp.py
+++ plugins/gbp.py
@@ -45,17 +45,10 @@ def on_message(bot, channel, user, message):
     match = [s.strip() for s in re.findall(__match__, message)[0]]
     _, check_user, _, give_user = match
 
-    if check_user:
-        user_info = bot.call_api('users', 'info', user=check_user)
-        if not user_info['ok']:
-            return "No such user."
+    if not (give_user or check_user):
+        return
 
-        username = user_info['user']['name']
-        user_id = user_info['user']['id']
-
-        return "*{}*: {} _gbp_".format(username, get_gbp(user_id))
-
-    elif give_user:
+    if give_user:
         user_info = bot.call_api('users', 'info', user=give_user)
 
         if not user_info['ok']:
@@ -74,4 +67,16 @@ def on_message(bot, channel, user, message):
             if give_user == bot.admin_id:
                 decrement(user)
             decrement(give_user)
+
+    if check_user:
+        if not user_info:
+            user_info = bot.call_api('users', 'info', user=check_user)
+
+        if not user_info['ok']:
+            return "No such user."
+
+        username = user_info['user']['name']
+        user_id = user_info['user']['id']
+
+        return "*{}*: {} _gbp_".format(username, get_gbp(user_id))
 
